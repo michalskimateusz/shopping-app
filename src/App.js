09ea12f66f8094986690900b278c407e31b5fab9
Mateusz Michalski <michalskimateusz@outlook.com>
2021-10-17 21:39:52 +0200

    adding components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing}/>
+          <Route path="/product/:productId" exact component={ProductDetail}/>
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
